be281004e7de27ab756384375af3831a1836b1aa
Travis Rich <travis.s.rich@gmail.com>
2017-02-24 18:57:28 -0500

    Remove submitExternal

--- routes/dinosaur.js
+++ routes/dinosaur.js
@@ -30,22 +30,23 @@ app.post('/dinosaur', postDinosaur);
 
 export function postBeef(req, res, next) {
 	console.log(req.body);
-	const url = process.env.IS_PRODUCTION_API === 'TRUE'
-		? `https://www.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&foo=bar`
-		: `https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&workerId=${req.body.workerId}&foo=bar`; // HTTPS is failing here, even though the docs mandate it. No clue...
+	// const url = process.env.IS_PRODUCTION_API === 'TRUE'
+	// 	? `https://www.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&foo=bar`
+	// 	: `https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&workerId=${req.body.workerId}&foo=bar`; // HTTPS is failing here, even though the docs mandate it. No clue...
 
-	return request({
-		method: 'POST',
-		uri: url,
-		form: {
-			assignmentId: req.body.assignmentId,
-			foo: 'bar',
-		}
-	})
-	.then(function(amazonSubmitResponse) {
-		console.log('HIT Submitted ', JSON.stringify(amazonSubmitResponse, null, 2));
-		return mturk.createClient(mturkConfig);
-	})
+	// return request({
+	// 	method: 'POST',
+	// 	uri: url,
+	// 	form: {
+	// 		assignmentId: req.body.assignmentId,
+	// 		foo: 'bar',
+	// 	}
+	// })
+	// .then(function(amazonSubmitResponse) {
+	// 	console.log('HIT Submitted ', JSON.stringify(amazonSubmitResponse, null, 2));
+	// 	return mturk.createClient(mturkConfig);
+	// })
+	return mturk.createClient(mturkConfig)
 	.then(function(mturkClient) {
 		console.log('this is my assignment id', req.body.assignmentId);
 		return mturkClient.req('ApproveAssignment', { AssignmentId: req.body.assignmentId });	
