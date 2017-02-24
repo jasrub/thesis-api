818ac2178c1dbc9463009dbe21c952eabb772c5d
Travis Rich <travis.s.rich@gmail.com>
2017-02-24 17:40:34 -0500

    Try changing varible name

--- routes/dinosaur.js
+++ routes/dinosaur.js
@@ -30,15 +30,15 @@ app.post('/dinosaur', postDinosaur);
 export function postBeef(req, res, next) {
 	console.log(req.body);
 	const url = process.env.IS_PRODUCTION_API === 'TRUE'
-		? `https://www.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&completed=true`
-		: `https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&completed=true`;
+		? `https://www.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&foo=true`
+		: `https://workersandbox.mturk.com/mturk/externalSubmit?assignmentId=${req.body.assignmentId}&foo=true`;
 
 	return request({
 		method: 'POST',
 		uri: url,
 		form: {
 			assignmentId: req.body.assignmentId,
-			completed: true,
+			foo: true,
 		}
 	})
 	.then(function(amazonSubmitResponse) {
